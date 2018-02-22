3afc3f25813443d140e6fd657ebefe3d034f0ba3
LACY HOGAN <lacy.hogan@gmail.com>
2018-02-21 16:16:16 -0800

    Added JSON Local Storage

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ Product.chartDrawn = false;
 Product.allProducts = [];
 Product.compareProducts = [];
 Product.clicks = [];
+Product.names = ['r2d2', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'duck-nose', 'dragon-meat', 'pen', 'sweeper', 'scissors', 'shark', 'sweeper', 'sauntaun', 'unicorn-meat', 'tentacle', 'watering-can', 'wine-glass'];
 
 //Constructor Function
 
@@ -23,31 +24,6 @@ function Product (name, filepath) {
   this.views = 0;
   Product.allProducts.push(this);
 }
-
-//Instances
-
-Product.names = ['R2D2', 'Banana Cutter', 'Bathroom', 'Rain Boots', 'Breakfast', 'Bubblegum', 'Reverse Chair', 'Cthulhu', 'Doggy Duck Nose', 'Dragon Meat', 'Pen Utencil', 'Pet Sweeper', 'Pizza Scissors', 'Shark Sleeping Bag', 'Baby Sweeper', 'Tauntaun', 'Unicorn Meat', 'USB Tentacle', 'Self Watering Can', 'Wine Glass'];
-
-new Product(Product.names[0], 'img/bag.jpg');
-new Product(Product.names[1], 'img/banana.jpg');
-new Product(Product.names[2], 'img/bathroom.jpg');
-new Product(Product.names[3], 'img/boots.jpg');
-new Product(Product.names[4], 'img/breakfast.jpg');
-new Product(Product.names[5], 'img/bubblegum.jpg');
-new Product(Product.names[6], 'img/chair.jpg');
-new Product(Product.names[7], 'img/cthulhu.jpg');
-new Product(Product.names[8], 'img/dog-duck.jpg');
-new Product(Product.names[9], 'img/dragon.jpg');
-new Product(Product.names[10], 'img/pen.jpg');
-new Product(Product.names[11], 'img/pet-sweep.jpg');
-new Product(Product.names[12], 'img/scissors.jpg');
-new Product(Product.names[13], 'img/shark.jpg');
-new Product(Product.names[14], 'img/sweep.png');
-new Product(Product.names[15], 'img/tauntaun.jpg');
-new Product(Product.names[16], 'img/unicorn.jpg');
-new Product(Product.names[17], 'img/usb.gif');
-new Product(Product.names[18], 'img/water-can.jpg');
-new Product(Product.names[19], 'img/wine-glass.jpg');
 
 //Function Declarations
 
@@ -91,6 +67,7 @@ function clickOnProducts (event) {
     Product.allThreePics.style.display = 'none';
     updateProductArrays();
     drawChart();
+    localStorage.setItem('productsArray', JSON.stringify(Product.allProducts));
   }
   Product.totalClicks += 1;
 
@@ -166,5 +143,30 @@ function drawChart () {
 
 //Execution, Event Listeners
 
+if (localStorage !== null) {
+  var retrieveData = localStorage.getItem('productsArray');
+  Product.allProducts = JSON.parse(retrieveData);
+} else {
+  new Product('r2d2', 'img/bag.jpg');
+  new Product('banana', 'img/banana.jpg');
+  new Product('bathroom', 'img/bathroom.jpg');
+  new Product('boots', 'img/boots.jpg');
+  new Product('breakfast', 'img/breakfast.jpg');
+  new Product('bubblegum', 'img/bubblegum.jpg');
+  new Product('chair', 'img/chair.jpg');
+  new Product('cthulhu', 'img/cthulhu.jpg');
+  new Product('duck-nose', 'img/dog-duck.jpg');
+  new Product('dragon-meat', 'img/dragon.jpg');
+  new Product('pen', 'img/pen.jpg');
+  new Product('sweeper', 'img/pet-sweep.jpg');
+  new Product('scissors', 'img/scissors.jpg');
+  new Product('shark', 'img/shark.jpg');
+  new Product('sweeper', 'img/sweep.png');
+  new Product('sauntaun', 'img/tauntaun.jpg');
+  new Product('unicorn-meat', 'img/unicorn.jpg');
+  new Product('tentacle', 'img/usb.gif');
+  new Product('watering-can', 'img/water-can.jpg');
+  new Product('wine-glass', 'img/wine-glass.jpg');
+}
 displayPics();
 Product.allThreePics.addEventListener('click', clickOnProducts);
